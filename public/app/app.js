9ab21a5780616030ec2e74b3ba75b85b9f4105de
marek.kacprzak <marek.kacprzak@windowslive.com>
2017-04-24 21:25:01 +0200

    test main index - inline template

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,49 @@
 (function() {
 
-    var app = angular.module('app', [ngRoute]);
+    var app = angular.module('app', ['ngRoute']);
 
-    app.config(['$logProvider', function ($logProvider) {
+    app.config(['$logProvider', '$routeProvider', function ($logProvider, $routeProvider) {
 
         $logProvider.debugEnabled(true);
-
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                controllerAs: 'home',
+                template: '<h1>This is an inline template</h1>'//'/app/templates/home.html'
+            });
+            /*
+            .when('/schools', {
+                controller: 'AllSchoolsController',
+                controllerAs: 'schools',
+                templateUrl: '/app/templates/allSchools.html'
+            })
+            .when('/classrooms', {
+                controller: 'AllClassroomsController',
+                controllerAs: 'classrooms',
+                templateUrl: '/app/templates/allClassrooms.html'
+            })
+            .when('/activities', {
+                controller: 'AllActivitiesController',
+                controllerAs: 'activities',
+                templateUrl: '/app/templates/allActivities.html',
+                resolve: {
+                    activities: function (dataService) {
+                        return dataService.getAllActivities();
+                    }
+                }
+            })
+            .when('/classrooms/:id', {
+                templateUrl: '/app/templates/classroom.html',
+                controller: 'ClassroomController',
+                controllerAs: 'classroom'
+            })
+            .when('/classrooms/:id/detail/:month?', {
+                templateUrl: '/app/templates/classroomDetail.html',
+                controller: 'ClassroomController',
+                controllerAs: 'classroom'
+            })
+            .otherwise('/');
+            */
     }]);
-
+ 
 }());
